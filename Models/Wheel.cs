c082b1abd41014c577ad94c45c953bfba9767c6f
Dave Poole <dapoole1371@gmail.com>
2021-03-20 00:46:59 -0400

    Added second pay line

--- Models/Wheel.cs
+++ Models/Wheel.cs
@@ -5,19 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Slot_Machine.Models
 {
     public class Wheel
     {
         public int WheelID { get; set; }
-        public int Value1 { get; set; }
-        public int Value2 { get; set; }
-        public int Value3 { get; set; }
-        public int Value4 { get; set; }
+        public int Val1 { get; set; }
+        public int Val2 { get; set; }
+        public int Val3 { get; set; }
+        public int Val4 { get; set; }
 
-        public void Spin(Wheel wheel)
+        public void SpinWheel(int wheel)
         {
-
+            Random random = new Random();
+            Val1 = Val2;
+            Val2 = Val3;
+            Val3 = Val4;
+            Val4 = random.Next(1, 5);
         }
     }
 }
